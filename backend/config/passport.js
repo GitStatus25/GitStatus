11d9203542122d74d33196731a039ac9fd07f814
Nico Bentley <admin@gitstatus.co>
2025-03-18 18:52:41 -0600

    Update passport configuration to use authService for user management

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
-const User = require('../models/User');
+const AuthService = require('../services/authService');
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -8,7 +8,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await User.findById(id);
+    const user = await AuthService.getUserById(id);
     done(null, user);
   } catch (err) {
     done(err, null);
@@ -23,23 +23,7 @@ passport.use(new GitHubStrategy({
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
-      let user = await User.findOne({ githubId: profile.id });
-      
-      if (!user) {
-        user = await User.create({
-          githubId: profile.id,
-          name: profile.displayName || profile.username,
-          username: profile.username,
-          email: profile.emails && profile.emails[0] ? profile.emails[0].value : null,
-          avatarUrl: profile.photos && profile.photos[0] ? profile.photos[0].value : null,
-          accessToken
-        });
-      } else {
-        // Update access token
-        user.accessToken = accessToken;
-        await user.save();
-      }
-      
+      const user = await AuthService.handleGitHubAuth(profile, accessToken);
       return done(null, user);
     } catch (err) {
       return done(err, null);
